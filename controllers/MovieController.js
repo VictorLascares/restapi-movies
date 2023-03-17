e868199ec12db8b5657527cb6d6caff334c92183
Victor Lascares <vmlascares@gmail.com>
2023-03-16 23:19:38 -0600

    Agregando funcionalidad del CRUD de peliculas e implementando la conexion a la base de datos en el archivo index

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -1,24 +1,70 @@
 import Movie from "../models/Movie.js";
 
-
-const addMovie = (req, res) => {
-
+const addMovie = async (req, res) => {
+    const newMovie = new Movie(req.body);
+    try {
+        const savedMovie = await newMovie.save();
+        res.json(savedMovie);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
 };
 
-const getMovies = (req, res) => {
-
+const getMovies = async (req, res) => {
+    const movies = await Movie.find();
+    res.json(movies);
 };
 
-const getOneMovie = (req, res) => {
-
+const getOneMovie = async (req, res) => {
+    const { id } = req.params;
+    const movie = await Movie.findById(id);
+    if (!movie) {
+        const error = new Error("No existe la pelicula");
+        return res.status(404).json({ msg: error.message });
+    }
+    res.json(movie);
 };
 
-const updateMovie = (req, res) => {
+const updateMovie = async (req, res) => {
+    const { id } = req.params;
+    const movie = await Movie.findById(id);
 
-}; 
+    if (!movie) {
+        const error = new Error("No existe la pelicula");
+        return res.status(404).json({ msg: error.message });
+    }
 
-const deleteMovie = (req, res) => {
+    // Update Movie
+    Object.keys(req.body).forEach(property => {
+        if (req.body[property]) {
+            movie[property] = req.body[property];
+        }
+    });
 
+    // Save updated movie
+    try {
+        const updatedMovie = await movie.save();
+        res.json(updatedMovie);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
+};
+
+const deleteMovie = async (req, res) => {
+    const { id } = req.params;
+    const movie = await Movie.findById(id);
+
+    if (!movie) {
+        const error = new Error("No existe la pelicula");
+        return res.status(404).json({ msg: error.message });
+    }
+
+    try {
+        const deletedMovie = await movie.deleteOne();
+        res.json(deletedMovie);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
 };
 
 export {
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/database.js';
 import movies from './routes/movies.js';
 const app = express();
+app.use(express.json());
 
 dotenv.config();
+
+connectDB();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
