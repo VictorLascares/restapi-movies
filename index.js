637e5eebf2c0aae8c27532478df1b45ce31ffb5a
Victor Lascares <vmlascares@gmail.com>
2023-03-16 22:10:15 -0600

    Actualizando path de index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import movies from './routes/movies.js';
 const app = express();
 
 dotenv.config();
@@ -7,7 +8,10 @@ dotenv.config();
 // Settings
 app.set('port', process.env.PORT || 3000);
 
+// Routes
+app.use('/movies',movies);
 
+// Satarting server
 app.listen(app.get('port'), () => {
     console.log(`Server listening on port ${app.get('port')}`);
 });
