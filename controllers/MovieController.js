1c0806be5afde7a1e43f10ee8864ffc30dc2e2a1
Victor Lascares <vmlascares@gmail.com>
2023-03-16 22:08:03 -0600

    Creando esqueleto del controlador de pelicula

---
+++ controllers/MovieController.js
@@ -0,0 +1,30 @@
+import Movie from "../models/Movie.js";
+
+
+const addMovie = (req, res) => {
+
+};
+
+const getMovies = (req, res) => {
+
+};
+
+const getOneMovie = (req, res) => {
+
+};
+
+const updateMovie = (req, res) => {
+
+}; 
+
+const deleteMovie = (req, res) => {
+
+};
+
+export {
+    addMovie,
+    getMovies,
+    getOneMovie,
+    updateMovie,
+    deleteMovie
+};
